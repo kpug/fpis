ec18b1c649155df32abcd5b7c0718d934bc61597
cameo-js <kjs8469@gmail.com>
2017-01-12 00:55:52 +0900

    1차 작업

---
+++ source/chapter2/MyModule.scala
@@ -0,0 +1,13 @@
+object MyModule {
+  def abs(n: Int): Int =
+    if (n < 0) -n
+    else n
+
+  private def formatAbs(x: Int) = {
+    val msg = "The absolute value of %d is %d"
+    msg.format(x, abs(x))
+  }
+
+  def main(args: Array[String]): Unit =
+    println(formatAbs(-42))
+}
