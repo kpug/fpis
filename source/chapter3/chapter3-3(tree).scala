f74cda869ce0749bd6beeeb3112fe1205149ea91
astonshnam <drumhyun@naver.com>
2017-01-18 22:01:27 +0900

    chap3-3
    create chapter3-3.asc
    create chapter3-3(tree).scala

---
+++ source/chapter3/chapter3-3(tree).scala
@@ -0,0 +1,23 @@
+object Tree {
+
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 1
+    case Branch(l,r) => 1 + size(l) + size(r)
+  }
+
+  def maximum(t: Tree[Int]): Int = t match {
+    case Leaf(n) => n
+    case Branch(l,r) => maximum(l) max maximum(r)
+  }
+
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 0
+    case Branch(l,r) => 1 + (depth(l) max depth(r))
+  }
+
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(a) => Leaf(f(a))
+    case Branch(l,r) => Branch(map(l)(f), map(r)(f))
+  }
+
+}
