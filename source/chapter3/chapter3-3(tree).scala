a74d08afc5387756b517eb34ad1e41325d02d2ab
astonshnam <drumhyun@naver.com>
2017-01-19 13:43:30 +0900

    modify chapter3-3(tree).scala
    modify chapter3-3(tree).scala

--- source/chapter3/chapter3-3(tree).scala
+++ source/chapter3/chapter3-3(tree).scala
@@ -1,3 +1,9 @@
+package fpinscala.datastructures
+
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
 object Tree {
 
   def size[A](t: Tree[A]): Int = t match {
@@ -20,4 +26,20 @@ object Tree {
     case Branch(l,r) => Branch(map(l)(f), map(r)(f))
   }
 
+  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = t match {
+    case Leaf(a) => f(a)
+    case Branch(l,r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
+  
+  def sizeViaFold[A](t: Tree[A]): Int = 
+    fold(t)(a => 1)(1 + _ + _)
+  
+  def maximumViaFold(t: Tree[Int]): Int = 
+    fold(t)(a => a)(_ max _)
+  
+  def depthViaFold[A](t: Tree[A]): Int = 
+    fold(t)(a => 0)((d1,d2) => 1 + (d1 max d2))
+
+  def mapViaFold[A,B](t: Tree[A])(f: A => B): Tree[B] = 
+    fold(t)(a => Leaf(f(a)): Tree[B])(Branch(_,_))  
 }
